d3171ae8208025d6879a29947132c86138a3f3a1
SebastianVelo <36246993+SebastianVelo@users.noreply.github.com>
2018-02-14 22:09:27 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,20 @@
-function Mostrar()
-{
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
+function Mostrar(){
+	//tomo el mes
+	var mes = document.getElementById('mes').value;
 	
-	
-
-
-
+	switch(mes){
+		case "Febrero":
+					  alert("Tiene 28 dias");
+					  break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+					  alert("Este tiene 30 dias");
+					  break;
+		default:
+					  alert("Este mes tiene 31 dias");
+					  break;	  
+		
+	}
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,11 @@
 function Mostrar()
 {
-//tomo la hora 
-var laHora = document.getElementById('hora').value;
-
-//alert (laHora);
-	
-	
-
-
-
+	//tomo la hora 
+	var laHora = document.getElementById('hora').value;
+	if(isNaN(laHora) || laHora > 23 || laHora < 0){
+		alert("Erroneo!!");
+	}
+	else if(laHora > 6 && laHora < 12) {
+		alert("Es de mañana");
+	}
 }//FIN DE LA FUNCIÓN
